d9476ef6d593b488d9ee1a8b40823b0074bfc002
Kazi-Tanvir <kazimainulislamtanvir@gmail.com>
2026-07-28 21:39:41 +0600

    Microservices E-commerce App Kafka Tutorial

---
+++ packages/kafka/src/client.ts
@@ -0,0 +1,9 @@
+import { Kafka } from "kafkajs";
+
+export const createKafkaClient = (services: string) => {
+    const kafka = new Kafka({
+        clientId: 'kafka',
+        brokers: ["localhost:9094", "localhost:9095", "localhost:9096"]
+    });
+    
+}
